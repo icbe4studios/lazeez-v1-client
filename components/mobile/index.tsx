9fa6e5c0dbfeb8206eff5509a52769a17ffb22fb
dsamithmendis <samithmendis.01@gmail.com>
2026-04-24 23:59:51 +0530

    feat: text

--- components/mobile/index.tsx
+++ components/mobile/index.tsx
@@ -24,9 +24,9 @@ export default function ComingSoonMobile() {
       </div>
 
       <div className="flex flex-col items-center justify-center flex-1">
-        <h2 className="text-3xl md:text-5xl tracking-widest mb-6 text-white drop-shadow-[0_0_20px_rgba(255,255,255,0.8)]">
-          FRESH TASTE{" "}
-          <span className="block md:inline italic font-bold drop-shadow-[0_0_5px_rgba(255,255,255,1)]">
+        <h2 className="text-3xl md:text-5xl tracking-widest mb-6 text-white">
+          <span className="text-shadow-soft">FRESH TASTE</span>{" "}
+          <span className="block md:inline italic font-bold text-shadow-strong">
             COMING SOON
           </span>
         </h2>
